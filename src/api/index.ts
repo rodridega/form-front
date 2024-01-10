57b3eba618f30fff5068da7e50d858f1d6e15bd3
Rodrigo Deganutti <56453875+rodridega@users.noreply.github.com>
2024-01-10 00:42:57 -0300

    add backend api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: 'http://localhost:3000'
-})
 /* const api = axios.create({
-    baseURL: 'https://form-production-f761.up.railway.app'
+    baseURL: 'http://localhost:3000'
 }) */
+const api = axios.create({
+    baseURL: 'https://form-production-f761.up.railway.app'
+})
 
 export default api
--- src/helpers/validateData.ts
+++ src/helpers/validateData.ts
@@ -1,4 +1,7 @@
-import { FormData } from "../types/FormData";
+export interface FormData {
+    [key: string]: any;
+    // Aquí puedes agregar otras propiedades específicas de tu interfaz si las tienes.
+}
 
 export function validateObject(formData: FormData): boolean {
     for (const key in formData) {
